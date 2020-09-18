47f59a27cd107861407b03aef4270da5db6eb462
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2020-09-18 23:02:26 +0100

    add deploy workflows

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient =  ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.rentwise.com.ng/',
       headers: req.headers
     });
   } else {
--- orders/src/app.ts
+++ orders/src/app.ts
@@ -17,7 +17,7 @@ app.set('trust proxy', true);
 app.use(json());
 app.use(cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: false
 }));
 
 app.use(currentUser);
