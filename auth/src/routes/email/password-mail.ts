40e20b9358a83e884101146e6db12f52e3659e99
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2020-10-12 15:01:34 +0100

    Update password-mail.ts

--- auth/src/routes/email/password-mail.ts
+++ auth/src/routes/email/password-mail.ts
@@ -17,7 +17,7 @@ export const email = async (req : Request, res : Response, next : NextFunction) 
     if (!user) {
       throw new NotAuthorizedError();
     }
-    const mg = mailgun({apiKey: '8486b5c05f2e14e06d75b11fc158354a-cb3791c4-70ca2230', domain: DOMAIN});
+    const mg = mailgun({apiKey: '', domain: DOMAIN});
 
         const data = {
             to: `${email}`, // admin
