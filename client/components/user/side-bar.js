aba80066ee81845f8eda6ab8cb4658ddf2aaa75f
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2020-10-15 13:13:14 +0100

    Payment card added

--- client/components/user/side-bar.js
+++ client/components/user/side-bar.js
@@ -4,89 +4,96 @@ import {Fragment} from 'react';
 
 const SideBar = ({currentuser}) => {
     return (
-        <Fragment>
-
-<div className="nk-sidebar nk-sidebar-fixed " data-content="sidebarMenu">
-        <div className="nk-sidebar-element nk-sidebar-head">
-          <div className="nk-sidebar-brand">
-            <a href="/">
-            <a className="logo-link nk-sidebar-logo">
-              <img
-                className="logo-light logo-img"
-                src="/assets/images/logoRent.PNG"
-                srcSet="/assets/images/logoRent.PNG 2x"
-                alt="logo"
-              />
-              <img
-                className="logo-dark logo-img"
-                src="/assets/images/logo-dark.png"
-                srcSet="/assets/images/logoRent.PNG 2x"
-                alt="logo-dark"
-              />
-            </a>
-            </a>
+      <Fragment>
+        <div
+          className="nk-sidebar nk-sidebar-fixed "
+          data-content="sidebarMenu"
+        >
+          <div className="nk-sidebar-element nk-sidebar-head">
+            <div className="nk-sidebar-brand">
+              <a href="/">
+                <a className="logo-link nk-sidebar-logo">
+                  <img
+                    className="logo-light logo-img"
+                    src="/assets/images/logoRent.PNG"
+                    srcSet="/assets/images/logoRent.PNG 2x"
+                    alt="logo"
+                  />
+                  <img
+                    className="logo-dark logo-img"
+                    src="/assets/images/logo-dark.png"
+                    srcSet="/assets/images/logoRent.PNG 2x"
+                    alt="logo-dark"
+                  />
+                </a>
+              </a>
+            </div>
+            <div className="nk-menu-trigger mr-n2"></div>
           </div>
-          <div className="nk-menu-trigger mr-n2"></div>
-        </div>
-        <div className="nk-sidebar-element">
-          <div className="nk-sidebar-body" data-simplebar>
-            <div className="nk-sidebar-content">
-              <div className="nk-sidebar-widget d-none d-xl-block"></div>
-              <div className="nk-sidebar-widget nk-sidebar-widget-full d-xl-none pt-0"></div>
+          <div className="nk-sidebar-element">
+            <div className="nk-sidebar-body" data-simplebar>
+              <div className="nk-sidebar-content">
+                <div className="nk-sidebar-widget d-none d-xl-block"></div>
+                <div className="nk-sidebar-widget nk-sidebar-widget-full d-xl-none pt-0"></div>
 
-              <div className="nk-sidebar-menu">
-                <ul className="nk-menu">
-                  
+                <div className="nk-sidebar-menu">
+                  <ul className="nk-menu">
+                    <li className="nk-menu-item">
+                      <Link href="/user/dashboard">
+                        <a href="/user/dashboard" className="nk-menu-link">
+                          <span className="nk-menu-icon">
+                            <em className="icon ni ni-dashboard"></em>
+                          </span>
+                          <span className="nk-menu-text">Home</span>
+                        </a>
+                      </Link>
+                    </li>
 
-                  <li className="nk-menu-item">
-                  <Link href="/user/dashboard">
-                    <a  href="/user/dashboard" className="nk-menu-link">
-                      <span className="nk-menu-icon">
-                        <em className="icon ni ni-dashboard"></em>
-                      </span>
-                      <span className="nk-menu-text">Home</span>
-                    </a>
-                    </Link>
-                  </li>
+                    <li className="nk-menu-item">
+                      <Link href="/user/plans">
+                        <a href="/user/plans" className="nk-menu-link">
+                          <span className="nk-menu-icon">
+                            <em className="icon ni ni-report-profit"></em>
+                          </span>
+                          <span className="nk-menu-text">Plans</span>
+                        </a>
+                      </Link>
+                    </li>
 
-                  <li className="nk-menu-item">
-                  <Link href="/user/plans">
-                    <a  href="/user/plans" className="nk-menu-link">
-                      <span className="nk-menu-icon">
-                        <em className="icon ni ni-report-profit"></em>
-                      </span>
-                      <span className="nk-menu-text">Plans</span>
-                    </a>
-                    </Link>
-                  </li>
+                    <li className="nk-menu-item">
+                      <a k href="/user/profile" className="nk-menu-link">
+                        <span className="nk-menu-icon">
+                          <em className="icon ni ni-user-c"></em>
+                        </span>
+                        <span className="nk-menu-text">My Account</span>
+                      </a>
+                    </li>
 
-                  <li className="nk-menu-item">
-                
-                    <a k href="/user/profile" className="nk-menu-link">
-                      <span className="nk-menu-icon">
-                        <em className="icon ni ni-user-c"></em>
-                      </span>
-                      <span className="nk-menu-text">My Account</span>
-                    </a>
-                  </li>
+                    <li className="nk-menu-item">
+                      <a href="" className="nk-menu-link">
+                        <span className="nk-menu-icon">
+                          <em className="icon ni ni-histroy"></em>
+                        </span>
+                        <span className="nk-menu-text">History</span>
+                      </a>
+                    </li>
 
-                  <li className="nk-menu-item">
-                    <a href="html/history.html" className="nk-menu-link">
-                      <span className="nk-menu-icon">
-                        <em className="icon ni ni-histroy"></em>
-                      </span>
-                      <span className="nk-menu-text">History</span>
-                    </a>
-                  </li>
-                </ul>
+                    <li className="nk-menu-item">
+                      <a href="/user/payment" className="nk-menu-link">
+                        <span className="nk-menu-icon">
+                          <em className="icon ni ni-money"></em>
+                        </span>
+                        <span className="nk-menu-text">Payment</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-        </Fragment>
-    )
+      </Fragment>
+    );
 
 
 }
