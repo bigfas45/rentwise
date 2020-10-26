eb9fe51822f10560bfd51d550932d54ec2f11883
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2020-10-26 11:23:00 +0100

    paystack webhook test

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,8 @@ const buildClient =  ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: 'http://www.rentwise.ng/',
+      baseURL:
+        'http://www.rentwise.ng/',
       headers: req.headers,
     });
   } else {
--- subscription/src/routes/webhook.ts
+++ subscription/src/routes/webhook.ts
@@ -10,13 +10,13 @@ import { Subscription } from '../models/subscription';
 
 const router = express.Router();
 
-router.get(
+router.post(
   '/api/subscription/my/webhook/subscription/',
   async (req: Request, res: Response) => {
     // Retrieve the request's body
     var event = req.body;
     // Do something with event
-    console.log("event", event);
+    console.log("event", event.data);
     res.status(200).send({event})
   }
 );
