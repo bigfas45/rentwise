b822e398962c047a79e7fd040d144b1afb6bd89a
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-05-22 09:37:08 +0100

    Mysql errors

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,6 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the servers
 
     return axios.create({
+      
       // baseURL:  'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       baseURL: 'http://www.rentwise.ng/',
       
