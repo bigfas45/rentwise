56f6899c7f671327ab9caecb63ab5908f0ae55a7
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-05-25 08:38:18 +0100

    updated mysql

--- subscription/src/models/mysql.ts
+++ subscription/src/models/mysql.ts
@@ -1,6 +1,6 @@
 import mysql from 'mysql';
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host      : 'nasdotcng.com',
     user      : 'notcsadm_otc',
     password  : 'N#M3IM^stG0' ,
@@ -10,11 +10,9 @@ const db = mysql.createConnection({
   
   // connect 
   // @ts-ignore
-  db.connect((err) => {
-    if (err){
-      console.log(err);
-    } 
-    console.log('Mysql 1 Connected ')
-  });
+db.getConnection(function(err, connection) {
+  // connected! (unless `err` is set)
+  connection.end();
+});
 
 export {db}
