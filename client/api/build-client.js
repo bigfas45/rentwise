0313a74002644ec42bd71a0f7a99193c9f704071
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-07-06 11:57:48 +0100

    resolve

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       // baseURL:  'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: 'http://www.rentwise.ng/',
+      baseURL: 'https://www.rentwise.ng/',
       headers: req.headers,
     });
   } else {
